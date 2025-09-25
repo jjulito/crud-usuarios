4a88f59446e0e77e1bcf64bc5f7e9b4eedd0b312
jjulito <134748148+jjulito@users.noreply.github.com>
2025-09-25 00:21:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("API CRUD Usuarios funcionando correctamente");
+});
